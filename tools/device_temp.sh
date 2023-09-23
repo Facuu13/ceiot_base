efb38f0ddf39e45a161592fc0200160bc3312bc0
Facundo Andrioli Villa <facundo.andrioli@keylab.com.ar>
2023-09-23 13:22:27 -0300

    se agregaron comentarios donde se explica el funcionamiento de la script

--- tools/device_temp.sh
+++ tools/device_temp.sh
@@ -3,19 +3,33 @@
 # Tu clave de API de OpenWeatherMap
 API_KEY="2287fb577d35a990fdbaa47ff609c6f4"
 
-# Realiza una solicitud GET de prueba a la API de OpenWeatherMap (ejemplo de ciudad: Nueva York)
-response=$(curl -s "http://api.openweathermap.org/data/2.5/forecast?q=New+York&appid=$API_KEY")
+# Lista de ciudades para elegir aleatoriamente
+ciudades=("Buenos+Aires" "Cordoba" "Rosario" "Mendoza" "La+Plata" "Mar+del+Plata" "Salta" "San+Juan" "San+Luis" "San+Miguel+de+Tucuman")
+
+# Elegir una ciudad aleatoria de la lista
+ciudad_aleatoria=${ciudades[$RANDOM % ${#ciudades[@]}]} # Me va a generar un numero aleatorio entre 0 y la cantidad de ciudades
+
+# Realiza una solicitud GET de prueba a la API de OpenWeatherMap con la ciudad aleatoria
+response=$(curl -s "http://api.openweathermap.org/data/2.5/forecast?q=$ciudad_aleatoria&appid=$API_KEY") # -s para no mostrar informacion adicional
+# En response se almacena la respuesta de la solicitud
 
 # Verifica si la solicitud fue exitosa (código de estado HTTP 200)
 if [ $? -eq 0 ]; then
-    # Usa jq para extraer la temperatura actual (en Kelvin) del primer elemento en la lista 'list'
-    temperatura_kelvin=$(echo "$response" | jq -r '.list[0].main.temp')
-
-    # Convierte la temperatura a grados Celsius
-    temperatura_celsius=$(echo "$temperatura_kelvin - 273.15" | bc)
-
-    echo "Temperatura actual en Nueva York: ${temperatura_celsius}°C"
+    # Si el código de salida es 0, significa que la solicitud fue exitosa
+    # Extraer la temperatura de la respuesta
+    temperatura_celsius=$(echo "$response" | jq -r '.list[0].main.temp')
+    # jq es una herramienta diseñada para procesar y extraer datos de documentos JSON.
+    # La opción -r se usa para obtener el valor de manera "cruda", lo que significa que elimina 
+    # cualquier formato adicional alrededor del valor que se extrae del documento JSON
+    
+    # Convertir la temperatura a Celsius
+    temperatura_celsius=$(echo "scale=2; $temperatura_celsius - 273.15" | bc)
+    #scale se usa para que el resultado tenga en este caso 2 decimales.
+    # bc se utiliza en esta línea para calcular la expresión matemática, 
+    # asegurando que el resultado tenga una precisión de 2 decimales
+    
+    # Mostrar la temperatura y la ciudad
+    echo "Temperatura actual en $ciudad_aleatoria: $temperatura_celsius°C"
 else
     echo "Error al realizar la solicitud a la API de OpenWeatherMap."
 fi
-
